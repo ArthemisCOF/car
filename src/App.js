7767eca408bf4a699d2cf796d8ba6cd88dd811a2
ArthemisCOF <Sorawit.Ily@hotmail.com>
2024-04-10 13:54:03 +0700

    setup router and bootstrap

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import HomePage from './pages/HomePage'
+import Car from './pages/Car';
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <HomePage />
+    },
+    {
+        path: '/car',
+        element: <Car />
+    }
+]);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <button type="button" class="btn btn-primary">Primary</button>
-      </header>
+    <div>
+       <RouterProvider router={router} />
     </div>
   );
 }
